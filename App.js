3e4b508165eef8341f384d33c8dfd10d4234fd21
SrGitQ <jmbalanzar@gmail.com>
2021-07-27 19:27:29 -0500

    new courses screen added, add navigation and redux

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { Home, Login, Edit, Pay } from './screens'
+import { Home, Login, Edit, Pay, Course } from './screens'
 
 const Stack = createStackNavigator();
 
@@ -14,12 +14,13 @@ const App = () => {
 		  	screenOptions={{
 				  headerShown:false
 			  }}
-			initialRouteName={"Pay"}
+			initialRouteName={"Course"}
 		  >
 			  <Stack.Screen name="Home" component={Home}/>
 			  <Stack.Screen name="Login" component={Login}/>
 			  <Stack.Screen name="Edit" component={Edit}/>
 			  <Stack.Screen name="Pay" component={Pay}/>
+			  <Stack.Screen name="Course" component={Course}/>
 		  </Stack.Navigator>
 	</NavigationContainer>
   );
